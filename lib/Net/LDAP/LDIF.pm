117bfc857c631563d88485651dcfa23cc07205f2
Graham Barr <gbarr@pobox.com>
2001-05-18 20:41:54 +0000

    Major updates to read/write methods with better error handling (From Robbie Allen)

--- lib/Net/LDAP/LDIF.pm
+++ lib/Net/LDAP/LDIF.pm
@@ -9,7 +9,7 @@ use SelectSaver;
 require Net::LDAP::Entry;
 use vars qw($VERSION);
 
-$VERSION = "0.06";
+$VERSION = "0.07";
 
 my %mode = qw(w > r < a >>);
 
@@ -44,8 +44,12 @@ sub new {
 
   # Default the encoding of DNs to 'none' unless the user specifies
   $opt{'encode'} = 'none' unless exists $opt{'encode'};
-  
+
+  # Default the error handling to die 
+  $opt{'onerror'} = 'die' unless exists $opt{'onerror'};
+
   my $self = {
+    change => 0,
     changetype => "modify",
     modify => 'add',
     wrap => 78,
@@ -53,73 +57,206 @@ sub new {
     fh   => $fh,
     file => "$file",
     opened_fh => $opened_fh,
+    eof => 0,
   };
 
   bless $self, $pkg;
 }
-
-sub _read_one {
+  
+sub _read_lines {
   my $self = shift;
   my @ldif;
-  
+
   {
     local $/ = "";
     my $fh = $self->{'fh'};
-    my $ln = scalar <$fh>;
-    return unless defined($ln);
+    my $ln = $self->{_next_lines} || scalar <$fh>;
+    unless ($ln) {
+       $self->{_next_lines} = '';
+       $self->{_current_lines} = '';
+       $self->eof(1);
+       return;
+    }
     $ln =~ s/\n //sg;
     chomp($ln);
+    $self->{_current_lines} = $ln;
     chomp(@ldif = split(/^/, $ln));
+    $self->{_next_lines} = scalar <$fh> || '';
+    $self->eof(1) unless $self->{_next_lines};
   }
+
+  @ldif;
+}
+
+
+# _read_one() is deprecated and will be removed
+# in a future version
+*_read_one = \&_read_entry; 
+
+sub _read_entry {
+  my $self = shift;
+  my @ldif;
+  $self->_clear_error();
+  
+  @ldif = $self->_read_lines;
+  return unless @ldif;
   shift @ldif if @ldif && $ldif[0] !~ /\D/;
 
-  return unless @ldif > 1 && $ldif[0] =~ s/^dn:(:?) *//;
+  if (@ldif <= 1) {
+     $self->_error("LDIF entry is not valid", @ldif);
+     return;
+  }
+  elsif (not ( $ldif[0] =~ s/^dn:(:?) *//) ) {
+     $self->_error("First line of LDIF entry does not begin with 'dn:'", @ldif);
+     return;
+  }
 
   my $dn = shift @ldif;
 
   if (length($1)) {
-    require MIME::Base64;
+    eval { require MIME::Base64 };
+    if ($@) {
+      $self->_error($@, @ldif);
+      return;
+    }
     $dn = MIME::Base64::decode($dn);
   }
 
-  my @attr;
-  my $last = "";
-  my $vals = [];
-  my $line;
-  my $attr;
-  foreach $line (@ldif) {
-    $line =~ s/^([-;\w]+):\s*// && ($attr = $1) or next;
-
-    if ($line =~ s/^:\s*//) {
-      require MIME::Base64;
-      $line = MIME::Base64::decode($line);
-    }
-
-    if ($attr eq $last) {
-      push @$vals, $line;
-      next;
-    }
-    else {
-      $vals = [$line];
-      push(@attr,$last=$attr,$vals);
-    }
-  }
   my $entry = Net::LDAP::Entry->new;
   $entry->dn($dn);
-  $entry->add(@attr);
+
+  if ($ldif[0] =~ /^changetype:\s*/) {
+    my $changetype = $ldif[0] =~ s/^changetype:\s*//
+        ? shift(@ldif) : $self->{'changetype'};
+    $entry->changetype($changetype);
+
+    return $entry if ($changetype eq "delete");
+
+    unless (@ldif) {
+      $self->_error("LDAP entry is not valid",@ldif);
+      return;
+    }
+
+    while(@ldif) {
+      my $modify = $self->{'modify'};
+      my $modattr;
+      my $lastattr;
+      if($changetype eq "modify") {
+        unless ( (my $tmp = shift @ldif) =~ s/^(add|delete|replace):\s*([-;\w]+)// ) {
+          $self->_error("LDAP entry is not valid",@ldif); 
+          return;
+        }
+        $lastattr = $modattr = $2;
+        $modify  = $1;
+      }
+      my @values;
+      while(@ldif) {
+        my $line = shift @ldif;
+        my $attr;
+  
+        if ($line eq "-") {
+          $entry->$modify($lastattr, \@values)
+            if defined $lastattr;
+          undef $lastattr;
+          @values = ();
+          last;
+        }
+ 
+        $line =~ s/^([-;\w]+):\s*// and $attr = $1;
+        if ($line =~ s/^:\s*//) {
+          eval { require MIME::Base64 };
+          if ($@) {
+            $self->_error($@, @ldif);
+            return;
+          }
+          $line = MIME::Base64::decode($line);
+        }
+ 
+        if( defined($modattr) && $attr ne $modattr ) {
+          $self->_error("LDAP entry is not valid", @ldif);
+          return;
+        }
+
+        if(!defined($lastattr) || $lastattr ne $attr) {
+          $entry->$modify($lastattr, \@values)
+            if defined $lastattr;
+          $lastattr = $attr;
+          @values = ($line);
+          next;
+        }
+        push @values, $line;
+      }
+      $entry->$modify($lastattr, \@values)
+       if defined $lastattr;
+    }
+  }
+
+  else {
+    my @attr;
+    my $last = "";
+    my $vals = [];
+    my $line;
+    my $attr;
+    foreach $line (@ldif) {
+      $line =~ s/^([-;\w]+):\s*// && ($attr = $1) or next;
+  
+      if ($line =~ s/^:\s*//) {
+        eval { require MIME::Base64 };
+        if ($@) {
+          $self->_error($@, @ldif);
+          return;
+        }
+        $line = MIME::Base64::decode($line);
+      }
+  
+      if ($attr eq $last) {
+        push @$vals, $line;
+        next;
+      }
+      else {
+        $vals = [$line];
+        push(@attr,$last=$attr,$vals);
+      }
+    }
+    $entry->add(@attr);
+  }
+  $self->{_current_entry} = $entry;
+
   $entry;
 }
 
+sub read_entry {
+  my $self = shift;
+
+  unless ($self->{'fh'}) {
+     $self->_error("LDIF file handle not valid");
+     return;
+  }
+  $self->_read_entry();
+}
+
+# read() is deprecated and will be removed
+# in a future version
 sub read {
   my $self = shift;
 
-  return unless $self->{'fh'};
-  return _read_one($self) unless wantarray;
+  return $self->read_entry() unless wantarray;
 
   my($entry, @entries);
-  push(@entries,$entry) while $entry = _read_one($self);
+  push(@entries,$entry) while $entry = $self->read_entry;
 
-  return @entries;
+  @entries;
+}
+
+sub eof {
+  my $self = shift;
+  my $eof = shift;
+
+  if ($eof) {
+    $self->{_eof} = $eof; 
+  }
+
+  $self->{_eof};
 }
 
 sub _wrap {
@@ -179,189 +316,189 @@ sub _write_dn {
   print _wrap($dn,$wrap), "\n";
 }
 
+# write() is deprecated and will be removed
+# in a future version
 sub write {
   my $self = shift;
+
+  $self->{change} = 0;
+  $self->write_entry(@_);
+}
+
+sub write_entry {
+  my $self = shift;
   my $entry;
+  my $change = $self->{change};
   my $wrap = int($self->{'wrap'});
   local($\,$,); # output field and record separators
 
-  return unless $self->{'fh'};
+  unless ($self->{'fh'}) {
+     $self->_error("LDIF file handle not valid");
+     return;
+  }
   my $saver = SelectSaver->new($self->{'fh'});
   
   my $fh = $self->{'fh'};
   foreach $entry (@_) {
-    print "\n" if tell($self->{'fh'});
-    _write_dn($entry->dn,$self->{'encode'},$wrap);
-    _write_attrs($entry,$wrap);
+    unless (ref $entry) {
+       $self->_error("Entry '$entry' is not a valid Net::LDAP::Entry object.");
+       next;
+    }
+
+    if ($change) {
+      my @changes = $entry->changes;
+      my $type = $entry->changetype;
+
+      # Skip entry if there is nothing to write
+      next if $type eq 'modify' and !@changes;
+
+      print "\n" if tell($self->{'fh'});
+      _write_dn($entry->dn,$self->{'encode'},$wrap);
+
+      print "changetype: $type\n";
+
+      if ($type eq 'delete') {
+        next;
+      }
+      elsif ($type eq 'add') {
+        _write_attrs($entry,$wrap);
+        next;
+      }
+      elsif ($type eq 'modrdn') {
+        print _write_attr('newrdn',$entry->get_value('newrdn'),$wrap);
+        print 'deleteoldrdn: ',$entry->get_value('deleteoldrdn'),"\n";
+        my $ns = $entry->get_value('newsuperior');
+        print _write_attr('newsuperior',$ns,$wrap) if defined $ns;
+        next;
+      }
+
+      my $first = 0;
+      foreach my $chg (@changes) {
+        unless (ref($chg)) {
+          $type = $chg;
+          next;
+        }
+        print "-\n" if $first++;
+        my $i = 0;
+        while ($i < @$chg) {
+          my $attr = $chg->[$i++];
+          my $val = $chg->[$i++];
+          print $type,": ",$attr,"\n";
+          _write_attr($attr,$val,$wrap);
+        }
+      }
+    }
+
+    else {
+       print "\n" if tell($self->{'fh'});
+       _write_dn($entry->dn,$self->{'encode'},$wrap);
+       _write_attrs($entry,$wrap);
+    }
   }
 
   1;
 }
 
+# read_cmd() is deprecated in favor of read_entry() 
+# and will be removed in a future version
 sub read_cmd {
   my $self = shift;
 
-  return unless $self->{'fh'};
-  return _read_one_cmd($self) unless wantarray;
+  return $self->read_entry() unless wantarray;
 
   my($entry, @entries);
-  push(@entries,$entry) while $entry = _read_one_cmd($self);
+  push(@entries,$entry) while $entry = $self->read_entry;
 
-  return @entries;
+  @entries;
 }
 
-sub _read_one_cmd {
-  my $self = shift;
+# _read_one_cmd() is deprecated in favor of _read_one() 
+# and will be removed in a future version
+*_read_one_cmd = \&_read_entry;
 
-  my @ldif;
-
-  {
-    local $/ = "";
-    my $fh = $self->{'fh'};
-    my $ln = scalar <$fh>;
-    return unless defined $ln;
-    $ln =~ s/\n //sg;
-    chomp($ln);
-    chomp(@ldif = split(/^/, $ln));
-  }
-  shift @ldif if @ldif && $ldif[0] !~ /\D/;
-  return unless @ldif > 1 && $ldif[0] =~ s/^dn:(:?) *//;
-
-  my $dn = shift @ldif;
-
-  if (length($1)) {
-    require MIME::Base64;
-    $dn = MIME::Base64::decode($dn);
-  }
-
-  my $entry = Net::LDAP::Entry->new;
-  $entry->dn($dn);
-
-  my $line;
-  my $changetype = $ldif[0] =~ s/^changetype:\s*//
-	? shift(@ldif) : $self->{'changetype'};
-
-  $entry->changetype($changetype);
-
-  return $entry if ($changetype eq "delete");
-
-  return unless @ldif; # Bad LDIF
-
-  while(@ldif) {
-    my $modify = $self->{'modify'};
-    my $modattr;
-    my $lastattr;
-    if($changetype eq "modify") {
-      (my $tmp = shift @ldif) =~ s/^(add|delete|replace):\s*([-;\w]+)//
-	or return; # Bad LDIF
-      $lastattr = $modattr = $2;
-      $modify  = $1;
-    }
-    my @values;
-    while(@ldif) {
-      my $line = shift @ldif;
-      my $attr;
-
-      if ($line eq "-") {
-        $entry->$modify($lastattr, \@values)
-	  if defined $lastattr;
-        undef $lastattr;
-	@values = ();
-	last;
-      }
-
-	  $line =~ s/^([-;\w]+):\s*// and $attr = $1;
-	  if ($line =~ s/^:\s*//) {
-	    require MIME::Base64;
-	    $line = MIME::Base64::decode($line);
-	  }
-
-      if(defined($modattr)) {
-        warn "bad LDIF" unless $attr eq $modattr;
-      }
-
-      if(!defined($lastattr) || $lastattr ne $attr) {
-        $entry->$modify($lastattr, \@values)
-	  if defined $lastattr;
-        $lastattr = $attr;
-	@values = ($line);
-	next;
-      }
-      push @values, $line;
-    }
-    $entry->$modify($lastattr, \@values)
-      if defined $lastattr;
-    
-  }
-  $entry;
-}
-
+# write_cmd() is deprecated in favor of write_entry() 
+# and will be removed in a future version
 sub write_cmd {
   my $self = shift;
-  my $entry;
-  my $wrap = int($self->{'wrap'});
 
-  return unless $self->{'fh'};
-  my $saver = SelectSaver->new($self->{'fh'});
-  
-  foreach $entry (grep { defined } @_) {
-    my @changes = $entry->changes;
-    my $type = $entry->changetype;
-
-    # Skip entry if there is nothing to write
-    next if $type eq 'modify' and !@changes;
-
-    print "\n" if tell($self->{'fh'});
-
-    _write_dn($entry->dn,$self->{'encode'},$wrap);
-
-    print "changetype: $type\n";
-
-    if ($type eq 'delete') {
-      next;
-    }
-    elsif ($type eq 'add') {
-      _write_attrs($entry,$wrap);
-      next;
-    }
-    elsif ($type eq 'modrdn') {
-      print _write_attr('newrdn',$entry->get_value('newrdn'),$wrap);
-      print 'deleteoldrdn: ',$entry->get_value('deleteoldrdn'),"\n";
-      my $ns = $entry->get_value('newsuperior');
-      print _write_attr('newsuperior',$ns,$wrap) if defined $ns;
-      next;
-    }
-
-    my $change;
-    my $first = 0;
-    foreach $change (@changes) {
-      unless (ref($change)) {
-        $type = $change;
-	next;
-      }
-      print "-\n" if $first++;
-      my $i = 0;
-      while ($i < @$change) {
-        my $attr = $change->[$i++];
-        my $val = $change->[$i++];
-	print $type,": ",$attr,"\n";
-	_write_attr($attr,$val,$wrap);
-      }
-    }
-  }
+  $self->{change} = 1;
+  $self->write_entry(@_);
 }
 
 sub done {
   my $self = shift;
-  my $fh = $self->{'fh'};
-  close $fh if $fh && $self->{'opened_fh'};
-  delete $self->{'fh'};
+  if ($self->{fh}) {
+     if ($self->{opened_fh}) {
+       close $self->{fh};
+       undef $self->{opened_fh};
+     }
+     delete $self->{fh};
+  }
   1;
 }
 
+my %onerror = (
+  'die'   => sub {
+                my $self = shift;
+                require Carp;
+                $self->done;
+                Carp::croak($self->error(@_));
+             },
+  'warn'  => sub { 
+                my $self = shift;
+                require Carp; 
+                Carp::carp($self->error(@_)); 
+             },
+  'undef' => sub { 
+                my $self = shift;
+                require Carp; 
+                Carp::carp($self->error(@_)) if $^W; 
+             },
+);
+
+sub _error {
+   my ($self,$errmsg,@errlines) = @_;
+   $self->{_err_msg} = $errmsg;
+   $self->{_err_lines} = join "\n",@errlines;
+
+   scalar &{ $onerror{ $self->{onerror} } }($self,$self->{_err_msg}) if $self->{onerror};
+}
+
+sub _clear_error {
+  my $self = shift;
+
+  undef $self->{_err_msg};
+  undef $self->{_err_lines};
+}
+
+sub error {
+  my $self = shift;
+  $self->{_err_msg};
+}
+
+sub error_lines {
+  my $self = shift;
+  $self->{_err_lines};
+}
+
+sub current_entry {
+  my $self = shift;
+  $self->{_current_entry};
+}
+
+sub current_lines {
+  my $self = shift;
+  $self->{_current_lines};
+}
+
+sub next_lines {
+  my $self = shift;
+  $self->{_next_lines};
+}
+
 sub DESTROY {
-    my $self = shift;
-    $self->done();
+  my $self = shift;
+  $self->done();
 }
 
 1;
