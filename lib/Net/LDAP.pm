b41d616d086efa06050ca7a603514dca4e4a15c8
Graham Barr <gbarr@pobox.com>
2001-07-17 12:53:20 +0000

    Release 0.24

--- lib/Net/LDAP.pm
+++ lib/Net/LDAP.pm
@@ -22,7 +22,7 @@ use Net::LDAP::Constant qw(LDAP_SUCCESS
 			   LDAP_INAPPROPRIATE_AUTH
 			);
 
-$VERSION 	= 0.23_03;
+$VERSION 	= 0.24;
 @ISA     	= qw(Net::LDAP::Extra);
 $LDAP_VERSION 	= 2;      # default LDAP protocol version
 
