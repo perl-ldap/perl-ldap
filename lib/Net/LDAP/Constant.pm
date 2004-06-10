33f75c31f902d63f6c8738dd69fa680bdc9b9db8
Peter Marschall <peter@adpm.de>
2004-06-10 09:31:41 +0000

    add LDAP_EXTENSION_PASSWORD_MODIFY

--- lib/Net/LDAP/Constant.pm
+++ lib/Net/LDAP/Constant.pm
@@ -478,7 +478,11 @@ extension OIDs.
 
 =item LDAP_EXTENSION_START_TLS (1.3.6.1.4.1.1466.20037)
 
-Indicates if the server supports the Start TLS extension (RFC-2830)
+Indicates if the server supports the Start TLS extension (RFC 2830)
+
+=item LDAP_EXTENSION_PASSWORD_MODIFY (1.3.6.1.4.1.4203.1.11.1)
+
+Indicates that the server supports the Password Modify extension (RFC 3062)
 
 =back
 
