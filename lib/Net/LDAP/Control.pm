c5cdc693975648367ec3d884f2f06c2ea81399a9
Peter Marschall <peter@adpm.de>
2006-09-03 21:16:33 +0000

    improve documentation

--- lib/Net/LDAP/Control.pm
+++ lib/Net/LDAP/Control.pm
@@ -282,6 +282,15 @@ for this method is to return TRUE if there is no error, but sub-classes may over
 =head1 SEE ALSO
 
 L<Net::LDAP>
+L<Net::LDAP::Control::EntryChange>
+L<Net::LDAP::Control::ManageDsaIT>
+L<Net::LDAP::Control::Paged>
+L<Net::LDAP::Control::PersistentSearch>
+L<Net::LDAP::Control::ProxyAuth>
+L<Net::LDAP::Control::Sort>
+L<Net::LDAP::Control::SortResult>
+L<Net::LDAP::Control::VLV>
+L<Net::LDAP::Control::VLVResponse>
 
 =head1 AUTHOR
 
