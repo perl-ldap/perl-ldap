d403aa750af36ad95087b7fa10ea3f1c7feac742
Graham Barr <gbarr@pobox.com>
2011-09-03 12:51:19 -0500

    Release 0.43

--- lib/Net/LDAP.pm
+++ lib/Net/LDAP.pm
@@ -28,7 +28,7 @@ use Net::LDAP::Constant qw(LDAP_SUCCESS
 			   LDAP_UNAVAILABLE
 			);
 
-$VERSION 	= "0.42";
+$VERSION 	= "0.43";
 @ISA     	= qw(Tie::StdHash Net::LDAP::Extra);
 $LDAP_VERSION 	= 3;      # default LDAP protocol version
 
