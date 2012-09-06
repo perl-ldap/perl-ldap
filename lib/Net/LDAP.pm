6fdb90e54aff2bfcfbb93dde9eb0327d6a2644f3
Peter Marschall <peter@adpm.de>
2012-09-06 22:30:54 +0200

    LDAP.pm: canonicalize host name with ldapi://
    Having the host name canonicalized allows one to call start_tls()
    with option verify => 'require', which does hostname checking.
    
    Confirmed to work for Quanah.

--- lib/Net/LDAP.pm
+++ lib/Net/LDAP.pm
@@ -271,7 +271,19 @@ sub connect_ldapi {
 		 : 120
   ) or return undef;
 
-  $ldap->{net_ldap_host} = 'localhost';
+  # try to get canonical host name [to allow start_tls on the connection]
+  require Socket;
+  if (Socket->can('getnameinfo') && Socket->can('getaddrinfo')) {
+    my @addrs;
+    my ($err,$host,$path) = Socket::getnameinfo($ldap->{net_ldap_socket}->peername, &Socket::AI_CANONNAME);
+
+    ($err, @addrs) = Socket::getaddrinfo($host, 0, { flags => &Socket::AI_CANONNAME } )
+      unless ($err);
+    map { $ldap->{net_ldap_host} = $_->{canonname}  if ($_->{canonname}) }  @addrs
+      unless ($err);
+  }
+
+  $ldap->{net_ldap_host} ||= 'localhost';
   $ldap->{net_ldap_peer} = $peer;
 }
 
